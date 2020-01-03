869b414b498ed0ff236e20f7b23fcd6cdd83b016
nick2905 <nicolasmanurung16@gmail.com>
2020-01-03 16:10:35 +0700

    second commit

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "nickappserver";
+    $user = "nick2905";
+    $pass = "ifi>2shop";
+    $db = "nickdb";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
